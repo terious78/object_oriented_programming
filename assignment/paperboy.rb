0aac21838896a013a85436665fda7cfb909c52b1
terious78 <taesuk.moon@gmail.com>
2016-10-20 19:42:42 -0400

    Final Ex. 2

--- assignment/paperboy.rb
+++ assignment/paperboy.rb
@@ -3,11 +3,12 @@
 class PaperBoy
 # 2. Create initialize method with several attributes;
   attr_reader :earnings
+  attr_accessor :name, :experience
 
-  def initialize(name, experience, earnings)
+  def initialize(name)
     @name        = name
-    @experience  = experience # => (The number of papers boy delivered)
-    @earnings    = earnings # => (Amount of money earned) READ-ONLY
+    @experience  = 0 # => (The number of papers boy delivered)
+    @earnings    = 0 # => (Amount of money earned) READ-ONLY
   end
 # 3. Day per paperboy gets
 #    Numbers of houses to deliver the paper
@@ -22,7 +23,7 @@ class PaperBoy
 #    'Quota : Calculateand return
 #             the boy's quota for next delivery'
   def quota
-    50 + @experience / 2
+    50 + (@experience / 2)
   end
 
 #    'Deliver : Take 2 house numbers and
@@ -31,15 +32,17 @@ class PaperBoy
 #               'start_address' = smaller number than the
 #               'end_address'
   def deliver(start_address, end_address)
-    experience = (end_address - (start_address - 1))
+    units = (end_address - start_address)
 
-    if experience < quota
-      (experience * 0.25 - 2.0)
-    elsif experience == quota
-      (experience * 0.25)
-    else experience > quota
-      (experience * 0.50)
+    if units < quota
+      pay = (units * 0.25) - 2.0
+    elsif units == quota
+      pay = units * 0.25
+    else units > quota
+      pay = units * 0.50
     end
+    @experience += units
+    @earnings += pay
   end
 
   def report
@@ -49,4 +52,7 @@ class PaperBoy
 
 end
 
-tommy = PaperBoy.new("Tommy", 0, 0)
+tommy = PaperBoy.new("Tommy")
+
+tommy.deliver(1, 50)
+tommy.report
